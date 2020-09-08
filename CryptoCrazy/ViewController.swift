635b2de4af73e0e1cf42171493d8bbc347d31e63
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2020-09-08 13:31:57 +0300

    Initial Commit

---
+++ CryptoCrazy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCrazy
+//
+//  Created by Oguzhan Bekir on 8.09.2020.
+//  Copyright © 2020 Oguzhan Bekir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
